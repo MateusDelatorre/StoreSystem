b09eb3deb7ac04bcb96b860fb28c438071f34bc7
MateusDelatorre <mateusSdelatorre18@gmail.com>
2021-05-31 21:12:50 -0300

    Conflito

--- src/Classes/Gerente.java
+++ src/Classes/Gerente.java
@@ -1,5 +1,53 @@
 package Classes;
 
-public class Gerente {
+public class gerente extends Funcionario {
 
+    private float lucroObtidoMes;
+
+    public gerente(float cpf, String nome, float salario, float lucroObtidoMes) {
+        super(cpf, nome, salario);
+        this.lucroObtidoMes = lucroObtidoMes;
+    }
+
+
+    public float getLucroObtidoMes() {
+        return lucroObtidoMes;
+    }
+    public void setLucroObtidoMes(float lucroObtidoMes) {
+        this.lucroObtidoMes = lucroObtidoMes;
+    }
+
+    ///////////////////
+    @Override
+    public void setCpf(float cpf) {
+        super.setCpf(cpf);
+    }
+    @Override
+    public void setNome(String nome) {
+        super.setNome(nome);
+    }
+
+    @Override
+    public void setSalario(float salario) {
+        super.setSalario(salario);
+    }
+
+
+    @Override
+    public float getCpf() {
+        return super.getCpf();
+    }
+
+    @Override
+    public String getNome(String nome) {
+        return super.getNome(nome);
+    }
+
+    @Override
+    public float getSalario() {
+        return super.getSalario();
+    }
+
+
+    
 }
--- src/Classes/Vendedor.java
+++ src/Classes/Vendedor.java
@@ -1,5 +1,57 @@
 package Classes;
 
-public class Vendedor {
+public class vendedor extends Funcionario {
+    private float vendasMes;
+    
+    
+
+    public vendedor(float cpf, String nome, float salario, float vendasMes) {
+        super(cpf, nome, salario);
+        this.vendasMes = vendasMes;
+    }
+
+    public float getVendasMes() {
+        return vendasMes;
+    }
+
+    public void setVendasMes(float vendasMes) {
+        this.vendasMes = vendasMes;
+    }
+
+
+    @Override
+    public void setCpf(float cpf) {
+        super.setCpf(cpf);
+    }
+
+    @Override
+    public void setNome(String nome) {
+        super.setNome(nome);
+    }
+
+    @Override
+    public void setSalario(float salario) {    
+        //tentando fazer os 10%  ao salario
+        float x;
+        x=(getVendasMes()*10)/100;
+        super.setSalario(salario+x);
+    }
+
+    @Override
+    public float getCpf() {
+        return super.getCpf();
+    }
+
+    @Override
+    public String getNome(String nome) {
+        return super.getNome(nome);
+    }
+
+    @Override
+    public float getSalario() {
+        return super.getSalario();
+    }
+
+
 
 }

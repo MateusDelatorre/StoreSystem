7f3fcfd5cd324f4e2278f35ea5b3171e3c7b8222
ZaghaRage <lorranlbm@gmail.com>
2021-06-04 13:34:37 -0300

    Update Produto.java

--- src/Classes/Produto.java
+++ src/Classes/Produto.java
@@ -4,12 +4,20 @@ public class Produto {
 
     private int codigo;
     private String nome;
+    private String categoria;
+    private int preco_compra;
+    private int preco_venda;
+    private int quantida_estoque;
     private String descricao;
 
     //construtor Produto
-    public Produto (int Codigo, String Nome, String Descricao) {
+    public Produto (int codigo, String nome, String descricao, String categoria, int preco_venda, int preco_compra, int quantida_estoque) {
         this.codigo = Codigo;
         this.nome = Nome;
+        this.categoria = Categoria;
+        this.preco_compra = Preco_Compra;
+        this.preco_venda = Preco_venda;
+        this.quantidade_estoque = Quantidade_Estoque;
         this.descricao = Descricao;
     }
 
@@ -17,24 +25,44 @@ public class Produto {
 
     public int getCodigo() {
         return this.codigo;
-
     }
     public void setCodigo(int c) {
         this.codigo = c;
     }
     public String getNome() {
         return this.nome;
-
     }
     public void setNome(String n) {
         this.nome = n;
     }
+    public String getCategoria() {
+        return this.categoria;
+    }
+    public void setCategoria(String categ) {
+        this.categoria = categ;
+    }
+    public int getpreco_compra() {
+        return this.preco_compra;
+    }
+    public void setpreco_compra(int precomp) {
+        this.preco_compra = precomp;
+    }
+    public int getpreco_venda() {
+        return this.preco_venda;
+    }
+    public void setpreco_venda(int prevend) {
+        this.preco_venda = prevend;
+    }
+    public int getquantidade_estoque() {
+        return this.quantidade_estoque;
+    }
+    public void setquantidade_estoque(int quant) {
+        this.quantidade_estoque = quant;
+    }
     public String getDescricao() {
         return this.descricao;
-
     }
     public void setDescricao(String d) {
         this.descricao = d;
-
     }
 }

d8c9fcdf5ae5b0112c95401be1cfc7b320127b59
LO1701 <lurian502@gmail.com>
2021-06-22 10:42:59 -0300

    renda e caixaFuncao
    fiz uma classe renda da loja por que como caixa e auditores tem acesso a isso acho que fica melhor assim

--- src/Funcoes/Caixa_funcao.java
+++ src/Funcoes/Caixa_funcao.java
@@ -5,12 +5,16 @@ import Classes.Caixa;
 public class Caixa_funcao extends Caixa {
     private float dinheiro_recebido;
 
-    //construtores
-     public Caixa_funcao(String nome, String cpf, float dinheiro_recebido) {
+    //construtor
+     public Caixa_funcao(String nome, String cpf, float dinheiro_recebido,float setRenda) {
         super(nome, cpf);
         this.dinheiro_recebido = dinheiro_recebido;
+        setRenda = dinheiro_recebido;
     }
 
+    
+
+    
 
     //get e set
     public float getDinheiro_recebido() {
---
+++ src/Funcoes/Renda_da_loja.java
@@ -0,0 +1,24 @@
+package Funcoes;
+
+public class Renda_da_loja {
+    private float renda;
+    
+    //construtores
+    public Renda_da_loja() {
+        
+    }
+    
+    public Renda_da_loja(float renda) {
+        this.renda = renda;
+    }
+
+    //get e set
+
+    public float getRenda() {
+        return renda;
+    }
+
+    public void setRenda(float renda) {
+        this.renda = renda;
+    }
+}

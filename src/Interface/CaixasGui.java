862fe2e425ef6bb4f6a29ba66b54d89608ec7e03
ZaghaRage <lorranlbm@gmail.com>
2021-07-12 21:56:09 -0300

    Caixa

--- src/Interface/CaixasGui.java
+++ src/Interface/CaixasGui.java
@@ -1,17 +1,36 @@
 package Interface;
 
-public class CaixasGui {
+import java.util.ArrayList;
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import handler.ProdutoHandler;
 
-    protected static void StartButtons(){
+import javax.swing.WindowConstants.EXIT_ON_CLOSE;
+public class CaixasGui extends GuiModel{
+
+
+    protected void StartButtons(){
+        buttons = new ArrayList<JButton>();
+        buttons.add(new JButton("Recebe Dinheiro"));
+        buttons.get(0).setBounds(320, 290, 100, 40);
+    }
+
+    protected void Focus(){
         
     }
 
-    protected static void Focus(){
+    private void AddButtons(){
         
     }
 
-    private static void AddButtons(){
-        
-    }
-
+}
+private void caixasButton(){
+    addButton(buttons.get(1), new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            Renda_da_loja.setRenda(Renda_daloja.getRenda + valor);
+        }
+    });
 }
